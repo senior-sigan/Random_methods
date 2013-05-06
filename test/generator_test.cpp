2a6fa1eaeb448d3ecc1b7be69a4fc323cc725119
over <over@over-debby>
2013-05-07 00:09:20 +0700

    *generator v2 (must work on 32bit systems)

--- test/generator_test.cpp
+++ test/generator_test.cpp
@@ -1,5 +1,7 @@
 #include "../gauss_generator.h"
 #include <math.h>
+#include <stdio.h>
 int main(){
-    generate_gauss_seq("test.txt", 10, 10000, 6364136223846793005L, 1442695040888963407L,((unsigned long)1)<<63);
+    printf("%u",((unsigned int)-1) );
+    generate_gauss_seq("test.txt", 10, 10000, 69069, 5,((unsigned int)-1));
 }
